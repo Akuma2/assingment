56a72e4e1ee78075fcf22135b83d143f9740400a
Akuma2 <Cjiyaba@gmail.com>
2017-05-29 00:14:17 +0100

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,17 @@
+package JavaApplication2; 
+import java.util.Scanner;
+
+
+public class JavaApplication2 {
+    
+      public static void main(String[] args) {
+          System.out.println("Enter a number");
+          Scanner x = new Scanner(System.in);
+                  int no = x.nextInt(); 
+            if((no%2) == 0){System.out.println("this num is even");
+            }
+            else{System.out.println("this number is odd");}
+      }
+      
+    
+}
